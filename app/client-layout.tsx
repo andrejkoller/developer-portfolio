822c2e962f90070eb08dc77b99c8a6d5386de970
Andrej Koller <andrejkoller@outlook.com>
2026-08-13 22:45:49 +0200

    refactor: remove ThemeProvider and ThemeSwitcher from layout components

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -2,23 +2,20 @@
 
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
-import { ThemeProvider } from "@/components/theme/theme-provider";
 
 export default function ClientLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ThemeProvider>
-      <div className="flex flex-col min-h-screen text-sm">
-        <Header />
-        <main
-          className="grow w-full max-w-175 min-h-screen mx-auto px-4"
-          aria-label="Main content"
-        >
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <div className="flex flex-col min-h-screen text-sm">
+      <Header />
+      <main
+        className="grow w-full max-w-175 min-h-screen mx-auto px-4"
+        aria-label="Main content"
+      >
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 }
