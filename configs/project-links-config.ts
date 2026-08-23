ecc2707b8b91da2f4cba2fff1bfca42cffda2cab
Andrej Koller <andrejkoller@outlook.com>
2026-08-23 02:02:19 +0200

    chore: remove unused WorksSection and related configurations

--- configs/project-links-config.ts
+++ configs/project-links-config.ts
@@ -12,22 +12,4 @@ export const projectLinksConfig = [
     url: "https://linktr.ee/athanasa",
     isExternal: true,
   },
-  {
-    key: "eternalbravery",
-    name: "Eternal Bravery",
-    url: "https://linktr.ee/eternalbravery",
-    isExternal: true,
-  },
-  {
-    key: "fadinghell",
-    name: "Fading Hell",
-    url: "https://linktr.ee/fadinghell",
-    isExternal: true,
-  },
-  {
-    key: "insaneprophet",
-    name: "Insane Prophet",
-    url: "https://linktr.ee/insaneprophet",
-    isExternal: true,
-  },
 ];
